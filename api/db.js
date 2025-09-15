537719fcc587d4f6953594d68381fd36600111af
francistalbot <francistalbot@hotmail.com>
2025-09-14 23:38:45 -0400

    small change to fit the structure propose.

---
+++ api/db.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+/**
+ * Configuration et connexion à MongoDB
+ */
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log(`MongoDB connecté: ${conn.connection.host}`);
+  } catch (error) {
+    console.error("Erreur de connexion MongoDB:", error.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
--- api/server.js
+++ api/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
 
+const connectDB = require("./db");
 const gamesRoutes = require("./routes/games");
 
 const app = express();
@@ -14,14 +15,8 @@ app.use(express.urlencoded({ extended: true }));
 let publicFolder = __dirname + "/public";
 app.use(express.static(publicFolder));
 
-// Connexion MongoDB
-mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("Connecté à MongoDB"))
-  .catch((err) => console.error("Erreur MongoDB :", err));
+// Connexion à la base de données
+connectDB();
 
 // Routes
 app.use("/games", gamesRoutes);
